d71ebda474348153f1ebab3cb3773ae4651d906a
anurag-360 <anurag.goswami@careers360>
2024-12-05 18:05:25 +0530

    filter dropdown api added

---
+++ cnext_backend/apps/utils/helpers/choices.py
@@ -0,0 +1,3 @@
+CONSUMPTION_TYPE= {'paid':"Paid", 'free':"Free"}
+PUBLISHING_TYPE= {'published':'Published', 'unpublished': 'Unpublished'}
+TOOL_TYPE= {'result_predictor':'Result Predictors','college_predictor': 'College Predictors','mba_college_predictor':'MBA College Predictors'}
