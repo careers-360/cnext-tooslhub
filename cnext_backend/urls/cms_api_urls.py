18f37e3faf52ccc4893a9e8893c310d296b55031
anurag-360 <anurag.goswami@careers360>
2024-12-05 12:00:45 +0530

    inital commit

---
+++ cnext_backend/urls/cms_api_urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+
+# urlpatterns = [
+#     path(
+#         "health",
+#         .HealthAPI.as_view(),
+#         name="api.mailer.healthcheck",
+#     ),
+    
+# ]
