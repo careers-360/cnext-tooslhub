b489504c861cbefcea5a61f773795247ec31091e
anurag-360 <anurag.goswami@careers360>
2024-12-05 19:29:22 +0530

    run.bash file changed

--- deploy/run.bash
+++ deploy/run.bash
@@ -1,6 +1,31 @@
+# mkdir -p /var/log/gunicorn
+# # mkdir -p /home/ubuntu/main/cnext-toolshub/sitemap
+# touch /var/log/gunicorn/debug.log
+# #nohup gunicorn cnext_backend.wsgi:application --name cnext --timeout 300 --workers=2 --threads=4 --bind=0.0.0.0:8081 --log-level=error --error-logfile /var/log/gunicorn/debug.log --log-file /var/log/gunicorn/debug.log  --capture-output  &
+# nohup uwsgi --http :8081 --module cnext_backend.wsgi:application --procname-prefix cnext --workers 4 --threads 2 --harakiri 5000 --max-requests 50000 --http-timeout 5000 --socket myapp.sock --buffer-size 65536 --vacuum --master --enable-threads --logto /var/log/gunicorn/debug.log &
+# tail -f /var/log/gunicorn/debug.log
+
+
+#!/bin/bash
+
+# Create necessary directories
 mkdir -p /var/log/gunicorn
 mkdir -p /home/ubuntu/main/cnext-toolshub/sitemap
 touch /var/log/gunicorn/debug.log
-#nohup gunicorn cnext_backend.wsgi:application --name cnext --timeout 300 --workers=2 --threads=4 --bind=0.0.0.0:8081 --log-level=error --error-logfile /var/log/gunicorn/debug.log --log-file /var/log/gunicorn/debug.log  --capture-output  &
-nohup uwsgi --http :8081 --module cnext_backend.wsgi:application --procname-prefix cnext --workers 4 --threads 2 --harakiri 5000 --max-requests 50000 --http-timeout 5000 --socket myapp.sock --buffer-size 65536 --vacuum --master --enable-threads --logto /var/log/gunicorn/debug.log &
+
+# Start the application
+nohup uwsgi --http :8081 \
+    --module cnext_backend.wsgi:application \
+    --procname-prefix cnext \
+    --workers 4 \
+    --threads 2 \
+    --max-requests 50000 \
+    --socket myapp.sock \
+    --buffer-size 65536 \
+    --vacuum \
+    --master \
+    --enable-threads \
+    --logto /var/log/gunicorn/debug.log &
+
+# Tail logs
 tail -f /var/log/gunicorn/debug.log
