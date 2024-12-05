fc52741d5e2689e63ef4341d719b349626910611
anurag-360 <anurag.goswami@careers360>
2024-12-05 14:43:34 +0530

    tools app added

--- cnext_backend/urls/cms_api_urls.py
+++ cnext_backend/urls/cms_api_urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from rank_predictor.api import controllers
+from rank_predictor.api import controllers as rp_controller
+from tools.api import controllers as tools_controller
 
 
 urlpatterns = [
-    path("", controllers.HealthCheck.as_view(), name="health_check"),
+    path("", rp_controller.HealthCheck.as_view(), name="health_check"),
+
+    # Tools App APIs
+    path("tools", tools_controller.HealthCheck.as_view(), name="health_check"),
+    # path("tools/filter", tools_controller.CMSToolsFilter.as_view(), name="cms_tools_filter"),
+
+    
 ]
