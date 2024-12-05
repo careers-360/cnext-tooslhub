fae0f74ffd483ae7daeab17aba705f25de5808a0
Sahil Vishwakarma <sahil.vishwakarma@careers360.in>
2024-12-05 18:24:36 +0530

    feat:docker file and build template

--- cnext_backend/apps/tools/helpers/helpers.py
+++ cnext_backend/apps/tools/helpers/helpers.py
@@ -20,13 +20,36 @@ class CustomPaginator(PageNumberPagination):
 class ToolsHelper():
     def __init__(self, request=None, **kwargs):
         self.request = request
+
+    def get_manage_predictor_tools_filter(self):
+        id =  self.request.query_params.get('name')
+        consumption_type = self.request.query_params.get('consumption_type')
+        published_status_web_wap = self.request.query_params.get('status')
+        published_status_app = self.request.query_params.get('app_status')
+
+        filter_data = dict()
+        if id:
+            filter_data['id'] = id
+        if consumption_type:
+            filter_data['consume_type'] = consumption_type
+        if published_status_app:
+            filter_data['app_status'] = published_status_app
+        if published_status_web_wap:
+            filter_data['status'] = published_status_web_wap
+
+        return filter_data
     
-    def get_predictor_tool_list(self):
+    def get_predictor_tool_list(self,filter_value):
 
         itemsOnPage = self.request.query_params.get('page_size',10)
         queryset = CPProductCampaign.objects.only(
             'id', 'name', 'price', 'offer_price', 'status', 'app_status', 'created', 'updated'
         ).order_by('-updated')
+        
+        if filter_value:
+            queryset = queryset.filter(**filter_value)
+
+        published_result_predictor = queryset.filter(type='result_predictor').count()
         paginator = CustomPaginator()
         paginator.page_size = itemsOnPage
         tools = paginator.paginate_queryset(queryset, self.request)
@@ -37,13 +60,13 @@ class ToolsHelper():
                 "name": tool.name,
                 "list_price":tool.price,
                 "offer_price":tool.offer_price,
-                "web/wap_status":"published" if tool.status else "unpublished",
+                "web_wap_status":"published" if tool.status else "unpublished",
                 "app_status": tool.app_status,
                 "created_date":self.get_humanize_date_format(tool.created),
-                "updated_date":self.get_humanize_date_format(tool.updated)  
+                "updated_date":self.get_humanize_date_format(tool.updated),
             })
             
-        return paginator.get_paginated_response(tools_data)
+        return {"total_published_result_predictor":published_result_predictor,"tools_list":paginator.get_paginated_response(tools_data)}
     
     @staticmethod
     def get_humanize_date_format(date):
