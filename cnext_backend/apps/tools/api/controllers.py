a875ffab470c551a9fcb5bcae4415a09b79be48c
Ayush <ayush.jhajriya@careers360.com>
2024-12-06 11:59:47 +0530

    rp_cms

--- cnext_backend/apps/tools/api/controllers.py
+++ cnext_backend/apps/tools/api/controllers.py
@@ -1,13 +1,11 @@
-from tools.helpers.helpers import ToolsHelper
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from utils.helpers.response import SuccessResponse,ErrorResponse
 from utils.helpers.custom_permission import ApiKeyPermission
 from rest_framework import status
 
-from tools.models import *
-
-from utils.helpers.choices import *
+from tools.helpers.helpers import ToolsHelper
+from tools.models import CPProductCampaign, Domain
+from utils.helpers.choices import TOOL_TYPE, CONSUMPTION_TYPE, PUBLISHING_TYPE
 
 
 class HealthCheck(APIView):
@@ -16,15 +14,17 @@ class HealthCheck(APIView):
         return SuccessResponse({"message": "Tools App runnning"}, status=status.HTTP_200_OK)
     
 class CMSToolsFilterAPI(APIView):
+    """
+    Pending
+    """
+
+    permission_classes = (ApiKeyPermission,)
 
     def get(self, request, version, format=None, **kwargs):
 
         try:
             result = dict()
-            tools_name = list(
-                    CPProductCampaign.objects.values('id', 'name')
-                )
-            
+            tools_name = list(CPProductCampaign.objects.values('id', 'name'))
             domain = list(Domain.objects.filter(is_stream = 1).values('id','name'))
 
             # Construct the response payload
@@ -41,8 +41,6 @@ class CMSToolsFilterAPI(APIView):
         except Exception as e:
             return ErrorResponse("An unexpected error occurred.", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         
-        
-
 
 class ManagePredictorToolAPI(APIView):
     """
@@ -51,14 +49,14 @@ class ManagePredictorToolAPI(APIView):
     version -> v1 required
     GET API ->api/1/cms/manage-tool/list
     """
-    permission_classes = (
-        ApiKeyPermission,
-    )
+    permission_classes = (ApiKeyPermission,)
+
     def get(self, request, version, format=None, **kwargs):
         try:
             helper = ToolsHelper(request=request)
             filter_value = helper.get_manage_predictor_tools_filter()
             data = helper.get_predictor_tool_list(filter_value)
             return SuccessResponse(data, status=status.HTTP_200_OK)
+
         except Exception as e:
             return ErrorResponse(e.__str__(), status=status.HTTP_400_BAD_REQUEST)
