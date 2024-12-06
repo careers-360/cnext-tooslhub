eabb89ca5f15e6b0defc3787a65b07fde1b59ddb
Sahil Vishwakarma <sahil.vishwakarma@careers360.in>
2024-12-06 11:32:21 +0530

    feat:- url fixs

--- cnext_backend/urls/cms_api_urls.py
+++ cnext_backend/urls/cms_api_urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from rank_predictor.api import controllers as rp_controller
 from tools.api import controllers as tools_controller
 
-cms_prefix = 'api/<int:version>/cms'
+cms_prefix = 'api/<int:version>/cms/manage-tool'
 
 urlpatterns = [
     path("", rp_controller.HealthCheck.as_view(), name='health_check'),
 
     # Tools App APIs
     path('tools', tools_controller.HealthCheck.as_view(), name='health_check'),
-    path(cms_prefix + '/manage-predictor-tool', tools_controller.ManagePredictorToolAPI.as_view(), name='predictor-tools-list'),
-    path(cms_prefix + '/tools/filter', tools_controller.CMSToolsFilterAPI.as_view(),name='tools-filter'),
+    path(cms_prefix + '/list', tools_controller.ManagePredictorToolAPI.as_view(), name='predictor-tools-list'),
+    path(cms_prefix + '/filter', tools_controller.CMSToolsFilterAPI.as_view(),name='tools-filter'),
 
 
 ]
