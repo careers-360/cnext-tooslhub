ac9e8a1546e6ce566fdfab615ffe3e79edbc051c
Ayush <ayush.jhajriya@careers360.com>
2024-12-10 16:27:37 +0530

    add API structure

--- cnext_backend/apps/rank_predictor/api/helpers.py
+++ cnext_backend/apps/rank_predictor/api/helpers.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from rank_predictor.models import RpContentSection
+from tools.models import CPProductCampaign, CollegeCourse
+
 
 class ResultPageStaticHelper:
 
@@ -19,3 +21,36 @@ class ResultPageStaticHelper:
             content["image_wap"] = f"{settings.CAREERS_BASE_IMAGES_URL}{content.get('image_wap')}"
 
         return content_data
+    
+    def _get_faq_section(self, product_id=None):
+        # Fetch faq section from database based on rp_id
+        faq_data = []
+
+        if not product_id:
+            return faq_data
+        
+        # FAQ table needs to be created and implemeted
+        faq_data = list(RpContentSection.objects.filter(product_id=product_id, status=1).values("id", "product_id", "heading", "content", "image_web", "image_wap", "updated"))
+        for faq_ in faq_data:
+            faq_["image_web"] = f"{settings.CAREERS_BASE_IMAGES_URL}{faq_.get('image_web')}"
+            faq_["image_wap"] = f"{settings.CAREERS_BASE_IMAGES_URL}{faq_.get('image_wap')}"
+
+        return faq_data
+    
+    def _get_exam_from_product(self, product_id=None):
+        exam_id = None
+        if product_id:
+            # Fetch exam id from product id from database
+            exam = CPProductCampaign.objects.filter(id=product_id).values("exam").first()
+            if exam:
+                exam_id = exam.get("exam")
+        return exam_id
+
+    def _get_colleges_from_exam(self, exam_id):
+        colleges = []
+        if exam_id:
+            colleges = CollegeCourse.objects.filter(exam=exam_id).values("college__id", "college__name").distinct()
+            for college_ in colleges:
+                college_["id"] = college_.pop("college__id", None)
+                college_["name"] = college_.pop("college__name", None)
+        return colleges
