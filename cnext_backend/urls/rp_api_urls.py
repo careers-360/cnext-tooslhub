763bcc8081bbcce9281f621d027783b9a2096829
Ayush <ayush.jhajriya@careers360.com>
2024-12-10 16:27:37 +0530

    add API structure

--- cnext_backend/urls/rp_api_urls.py
+++ cnext_backend/urls/rp_api_urls.py
@@ -5,4 +5,6 @@ from rank_predictor.api import controllers as rp_controller
 urlpatterns = [
     path("rank-predictor", rp_controller.HealthCheck.as_view(), name='rank_predictor_health_check'),
     path("api/<int:version>/rank-predictor/content-section", rp_controller.ContentSectionAPI.as_view(), name='content_section'),
+    path("api/<int:version>/rank-predictor/faq-section", rp_controller.FAQSectionAPI.as_view(), name='faq_section'),
+    path("api/<int:version>/rank-predictor/top-college", rp_controller.TopCollegesSectionAPI.as_view(), name='top_college'),
 ]
