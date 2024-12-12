ef92b49936150b86ae65c33934756b85455b79f1
Ayush <ayush.jhajriya@careers360.com>
2024-12-12 16:42:27 +0530

    feedback API

--- cnext_backend/urls/rp_api_urls.py
+++ cnext_backend/urls/rp_api_urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("rank-predictor", rp_controller.HealthCheck.as_view(), name='rank_predictor_health_check'),
     path("api/<int:version>/rank-predictor/content-section", rp_controller.ContentSectionAPI.as_view(), name='content_section'),
     path("api/<int:version>/rank-predictor/faq-section", rp_controller.FAQSectionAPI.as_view(), name='faq_section'),
+    path("api/<int:version>/rank-predictor/feedback-section", rp_controller.ReviewSectionAPI.as_view(), name='feedback_section'),
     path("api/<int:version>/rank-predictor/top-college", rp_controller.TopCollegesSectionAPI.as_view(), name='top_college'),
 ]
