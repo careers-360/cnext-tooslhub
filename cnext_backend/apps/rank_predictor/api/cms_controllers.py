b6d9401007ae5c31f0d752fd8b82631bffbe8adb
Ayush <ayush.jhajriya@careers360.com>
2024-12-17 13:41:34 +0530

    student appeared pannel

--- cnext_backend/apps/rank_predictor/api/cms_controllers.py
+++ cnext_backend/apps/rank_predictor/api/cms_controllers.py
@@ -51,3 +51,48 @@ class FlowTypeAPI(APIView):
             return CustomErrorResponse(msg, status=status.HTTP_400_BAD_REQUEST)
         else:
             return SuccessResponse({"message": msg}, status=status.HTTP_201_CREATED)
+
+
+class StudentAppearedAPI(APIView):
+    """
+    API for Student Appeared CMS Pannel
+    Endpoint : api/<int:version>/cms/rp/appeared-student
+    Params : product_id, year
+    """
+
+    permission_classes = [ApiKeyPermission]
+
+    def get(self, request, version, **kwargs):
+        product_id = request.GET.get('product_id')
+        year = request.GET.get('year')
+        if not product_id or not product_id.isdigit():
+            return CustomErrorResponse({"message": "product_id is required and should be a integer value"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if year and not str(year).isdigit():
+            return CustomErrorResponse({"message": "year should be a integer value"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        product_id = int(product_id)
+        # Fetch appeared student data from database and return it to client.
+        cms_helper = RPCmsHelper()
+        data = cms_helper._get_student_appeared_data(product_id=product_id, year=year)
+        return SuccessResponse(data, status=status.HTTP_200_OK)
+    
+    def post(self, request, version):
+        product_id = request.data.get('product_id')
+        year = request.data.get('year')
+        student_data = request.data.get('student_data')
+
+        if not product_id or not str(product_id).isdigit():
+            return CustomErrorResponse({"message": "product_id is required and should be a integer value"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if year and not str(year).isdigit():
+            return CustomErrorResponse({"message": "year should be a integer value"}, status=status.HTTP_400_BAD_REQUEST)
+
+        product_id = int(product_id)
+        # add appeared student data in database.
+        cms_helper = RPCmsHelper()
+        resp, data = cms_helper._add_student_appeared_data(student_data=student_data, product_id=product_id, year=year)
+        if resp:
+            return SuccessResponse(data, status=status.HTTP_201_CREATED)
+        else:
+            return CustomErrorResponse(data, status=status.HTTP_400_BAD_REQUEST)
--- cnext_backend/apps/rank_predictor/api/helpers.py
+++ cnext_backend/apps/rank_predictor/api/helpers.py
@@ -1,5 +1,7 @@
 from django.conf import settings
-from rank_predictor.models import RpContentSection, RpInputFlowMaster, RpResultFlowMaster
+from django.db.models import Max
+from datetime import datetime, timedelta
+from rank_predictor.models import RpContentSection, RpInputFlowMaster, RpResultFlowMaster, CnextRpSession
 from tools.models import CPProductCampaign, CollegeCourse, CPFeedback
 from .static_mappings import RP_DEFAULT_FEEDBACK
 
@@ -140,7 +142,6 @@ class RPCmsHelper:
 
         return result_flow_type_data
     
-
     def _add_flow_type(self, **kwargs):
         # Save input flow type data to database
         flow_type = kwargs.get("flow_type")
@@ -187,7 +188,6 @@ class RPCmsHelper:
 
         return False, "Failed to create, Incorrect flow type"
 
-
     def _delete_flow_type(self, flow_type, flow_id):
         # Delete input flow type from database
         if not flow_type or not flow_id:
@@ -203,3 +203,102 @@ class RPCmsHelper:
 
         return False, "Failed to delete, Incorrect flow type"
 
+    def _get_student_appeared_data(self, product_id, year):
+        if year:
+            year = int(year) 
+    
+        student_data = {
+            "product_id": product_id,
+            "year": year,
+            "count": 0,
+            "student_appeared_data": []
+        }
+        # Fetch student appeared data from database
+        rp_session_data = CnextRpSession.objects.filter(product_id=product_id, status=1)
+        if year:
+            rp_session_year_data = list(rp_session_data.filter(year=year).values("id", "product_id", "year", "session_date", "session_shift", "difficulty"))
+            if rp_session_year_data:
+                student_data["year"] = year
+                student_data["count"] = len(rp_session_year_data) 
+                student_data["student_appeared_data"] = rp_session_year_data 
+                return student_data
+        
+        if not student_data["student_appeared_data"]:
+            rp_max_year = rp_session_data.aggregate(Max('year'))['year__max']
+            rp_session_data = list(rp_session_data.filter(year=rp_max_year).values("id", "product_id", "year", "session_date", "session_shift", "difficulty"))
+            if rp_session_data:
+                student_data["year"] = rp_max_year 
+                student_data["count"] = len(rp_session_data) 
+                student_data["student_appeared_data"] = rp_session_data 
+
+        for item in student_data["student_appeared_data"]:
+            item["session_date"] = item["session_date"].strftime("%Y-%m-%d") if item.get("session_date") else None
+
+        return student_data
+    
+    def _add_student_appeared_data(self, student_data, product_id, year):
+        
+        if not year:
+            year = datetime.today().year
+        
+        if not product_id or not year or not isinstance(student_data, list):
+            return False, "Missing arguments or Incorrect data type"
+        
+        rp_session_ids = list(CnextRpSession.objects.filter(product_id=product_id, year=year).values_list("id",flat=True))
+        incomming_ids = [row_["id"] for row_ in student_data if row_.get("id")]
+        rp_session_mapping = {row_["id"]:row_ for row_ in student_data if row_.get("id")}
+
+        error = []
+        to_create = []
+        to_update = CnextRpSession.objects.filter(id__in=incomming_ids)
+
+        # Delete non existing records
+        non_common_ids = set(rp_session_ids) - set(incomming_ids)
+        CnextRpSession.objects.filter(product_id=product_id, year=year, id__in=non_common_ids).delete()
+
+        # Create new records
+        for new_row in student_data:
+            session_date = self.convert_str_to_datetime(new_row.get('session_date'))
+            session_shift = new_row.get('session_shift')
+            session_difficulty = new_row.get('session_difficulty')
+
+            if not session_date or not session_shift or not session_difficulty:
+                error.append({"row":new_row, "message": "Incorrect data"})
+                continue
+
+            if not new_row.get("id"):
+                to_create.append(CnextRpSession(product_id=product_id, year=year, session_date=session_date, session_shift=session_shift, difficulty=session_difficulty))
+        
+        if to_create:
+            CnextRpSession.objects.bulk_create(to_create)
+
+        # Update records
+        for row_ in to_update:
+            row_id = row_.id
+            session_date = rp_session_mapping[row_id].get("session_date") #datetime.strptime(row_["session_date"], '%Y-%m-%d').date()
+            session_shift = rp_session_mapping[row_id].get("session_shift")
+            session_difficulty = rp_session_mapping[row_id].get("session_difficulty")
+            if not session_date or not session_shift or not session_difficulty:
+                error.append({"id":row_id, "message": "Incorrect data"})
+                continue
+
+            row_.session_date = self.convert_str_to_datetime(session_date)
+            row_.session_shift = session_shift
+            row_.difficulty = session_difficulty
+
+        if incomming_ids:
+            CnextRpSession.objects.bulk_update(to_update, ["session_date", "session_shift", "difficulty"])
+        
+        final_output = {
+            "message": "Successfully created session",
+            "error": error,
+            "student_appeared_data": student_data,
+            "count": len(student_data) - len(error)
+        }
+        return True, final_output
+    
+    def convert_str_to_datetime(self, str_to_datetime):
+        try:
+            return datetime.strptime(str_to_datetime, '%Y-%m-%d') + timedelta(hours=6, minutes=31)
+        except ValueError:
+            return None
