7a53f79d3a5a1500f35be4f2a6cbdf3c120216ec
Ayush <ayush.jhajriya@careers360.com>
2024-12-17 14:58:50 +0530

    API naming fix

--- cnext_backend/urls/rp_cms_urls.py
+++ cnext_backend/urls/rp_cms_urls.py
@@ -4,5 +4,5 @@ from rank_predictor.api import cms_controllers as cms_controller
 
 urlpatterns = [
     path("api/<int:version>/cms/rp/flow-type", cms_controller.FlowTypeAPI.as_view(),  name='flow_type'),
-    path("api/<int:version>/cms/rp/student-appeared", cms_controller.StudentAppearedAPI.as_view(),  name='student_appeared'),
+    path("api/<int:version>/cms/rp/exam-session", cms_controller.ExamSessiondAPI.as_view(),  name='student_appeared'),
 ]
