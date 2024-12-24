5e439b46eb51d033e7b996d9f0cb47de167ecfa3
dnishad007 <deepak.opstree@careers360.in>
2024-12-24 12:13:54 +0530

    added header section uri

--- cnext_backend/apps/rank_predictor/api/controllers.py
+++ cnext_backend/apps/rank_predictor/api/controllers.py
@@ -4,7 +4,7 @@ from utils.helpers.response import SuccessResponse, CustomErrorResponse
 from utils.helpers.custom_permission import ApiKeyPermission
 from rest_framework import status
 from rank_predictor.api.helpers import InputPageStaticHelper
-from rank_predictor.helper.landing_helper import RPHelper
+from rank_predictor.helper.landing_helper import RPHelper, RPFormHelper
 
 class HealthCheck(APIView):
 
@@ -164,6 +164,36 @@ class HeaderSectionAPI(APIView):
             resp = {
                 "product_id" : product_id,
                 "content" : content,
+            }
+            return SuccessResponse(resp, status=status.HTTP_200_OK)
+        
+        no_such_product = f"product with id {product_id} doesnot exist"
+        return SuccessResponse(no_such_product, status=status.HTTP_404_NOT_FOUND)
+
+class FormSectionAPI(APIView):
+    """
+    API for Content Section on Result Page
+    Endpoint : api/<int:version>/rank-predictor/form-section
+    Params : product_id
+    """
+
+    permission_classes = [ApiKeyPermission]
+
+    def get(self, request, version, **kwargs):
+
+        product_id = request.GET.get('product_id')
+        if not product_id or not product_id.isdigit():
+            return CustomErrorResponse({"message": "product_id is required and should be a integer value"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        product_id = int(product_id)
+
+        # Fetch content from database and return it to client.
+        form_section_helper = RPFormHelper()
+        form_fields = form_section_helper._get_form_section(product_id=product_id)
+
+        if form_fields:
+            resp = {
+                "fields_list" : form_fields
             }
             return SuccessResponse(resp, status=status.HTTP_200_OK)
         
--- cnext_backend/apps/rank_predictor/helper/landing_helper.py
+++ cnext_backend/apps/rank_predictor/helper/landing_helper.py
@@ -1,4 +1,5 @@
 from tools.models import CPProductCampaign
+from rank_predictor.models import RpFormField
 
 class RPHelper:
 
@@ -10,4 +11,16 @@ class RPHelper:
         header_data = CPProductCampaign.objects.filter(id=product_id).values("id", "header_section", "custom_exam_name", "custom_flow_type", "custom_year", "video", "usage_count_matrix", "positive_feedback_percentage")
 
         return header_data
+    
+
+class RPFormHelper:
+
+    def __init__(self):
+        pass
+
+    def _get_form_section(self, product_id=None):
+
+        form_data = RpFormField.objects.filter(product_id=product_id).values("field_type", "input_flow_type", "display_name", "place_holder_text", "error_message", "weight", "mapped_process_type", "mandatory", "status")
+
+        return form_data
     
